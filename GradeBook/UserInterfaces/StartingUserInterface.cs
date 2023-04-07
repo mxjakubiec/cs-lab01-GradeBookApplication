55a44683aa795121e9034e051956b62967ada8b9
mxjakubiec <maksymilian.jakubiec@microsoft.wsei.edu.pl>
2023-04-07 18:53:20 +0200

    Make the BaseGradeBook class abstract (complete) +return a new GradeBook based on the provided type

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,20 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
+            if (parts[2] == "standard")
+            {
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if (parts[2] == "ranked")
+            {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", parts[2]);
+                return;
+            }
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
